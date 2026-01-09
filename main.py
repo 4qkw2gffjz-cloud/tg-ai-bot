2f65858d5adb262130df0e616c26a05e5b191fb9
4qkw2gffjz-cloud <4qkw2gffjz@privaterelay.appleid.com>
2026-01-09 23:56:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 import sys
 import types
+
+# фикс для Python 3.13
 sys.modules['imghdr'] = types.ModuleType('imghdr')
 
 import os
 from telegram import Update
-from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+from telegram.ext import (
+    ApplicationBuilder,
+    ContextTypes,
+    MessageHandler,
+    filters,
+)
 
 TOKEN = os.getenv("BOT_TOKEN")
 
+
 async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Бот работает 👋")
+    await update.message.reply_text("Бот жив 👋")
 
-app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
 
-print("Бот запущен")
-app.run_polling()
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
+
+    print("Бот запущен")
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
